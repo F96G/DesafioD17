659fe30e02bdbccd323539ece50ba1cb772972dc
fabio gazzoni <fabio_vdr_100@hotmail.com>
2022-01-17 22:50:13 -0300

    ya no se necesitan 2 listas para mensajeList

--- app/src/main/java/com/munidigital/bc2201/challenge2/MainViewModel.kt
+++ app/src/main/java/com/munidigital/bc2201/challenge2/MainViewModel.kt
@@ -9,7 +9,6 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
     private val _mensajeList = MutableLiveData<MutableList<String>>()
-    private val list = mutableListOf<String>()  //VER COMO NO UTILIZAR DOS LISTAS IGUALES
     private val respuestas = Respuestas()
     var yaRespondi = true
     val mensajeList: LiveData<MutableList<String>> get() =_mensajeList
@@ -19,8 +18,7 @@ class MainViewModel: ViewModel() {
     }
 
     fun refresh(){
-        list.clear()
-        _mensajeList.value = list
+        _mensajeList.value = mutableListOf()
         respuestas.reload()
     }
 
@@ -40,12 +38,11 @@ class MainViewModel: ViewModel() {
 
     private fun addMensaje(mensaje:String){
         val lista = mutableListOf<String>()
-        if (list.size > 0) {
-            for (i in 0..list.size - 1) {
-                lista.add(list.get(i))
+        if (_mensajeList.value?.size!! > 0) {
+            for (i in 0.._mensajeList.value?.size!! - 1) {
+                lista.add(_mensajeList.value!!.get(i))
             }
         }
-        list.add(mensaje)
         lista.add(mensaje)
 
         _mensajeList.value = lista
